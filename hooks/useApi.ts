80274161c014955ac7b1ae24c076aa88a13b11e9
Ashraf Alhaj <ashrfalhag.11@gmail.com>
2024-07-07 16:36:28 +0200

    revert: api base url

--- hooks/useApi.ts
+++ hooks/useApi.ts
@@ -6,7 +6,8 @@ export function useApi() {
 
 axios.defaults.headers["Content-Type"] = "application/json";
 axios.defaults.headers["Accept"] = 'application/json'
-axios.defaults.baseURL = "http://127.0.0.1:4000";
+axios.defaults.baseURL = "https://suhail.lilash.dev";
+  // axios.defaults.baseURL = "http://127.0.0.1:4000";
 
 
 
