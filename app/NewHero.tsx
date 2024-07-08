afcb4e9a5e02ff9242a50f3cadaab0c9490124bb
Ashraf Alhaj <ashrfalhag.11@gmail.com>
2024-07-08 13:19:50 +0200

    fix: spacing

--- app/NewHero.tsx
+++ app/NewHero.tsx
@@ -5,7 +5,7 @@ export default async function NewHero() {
 
     return (
 
-        <div className="relative h-screen lg:mt-20" id="home">
+        <div className="relative h-screen lg:mt-32" id="home">
     <div aria-hidden="true" className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
         <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
         <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
