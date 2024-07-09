2667537f913bb8752e2bca6a283c6555b6429cad
Ashraf Alhaj <ashrfalhag.11@gmail.com>
2024-07-09 11:59:14 +0200

    revert: api base url

--- providers/axiosClient.ts
+++ providers/axiosClient.ts
@@ -6,6 +6,6 @@ axios.defaults.headers["Accept"] = 'application/json'
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 // axios.defaults.baseURL = process.env.API_URL;
 // axios.defaults.baseURL = "http://134.122.87.199:4000";
-// axios.defaults.baseURL = "https://suhail-api.lilash.dev";
-axios.defaults.baseURL = "http://127.0.0.1:4000";
+axios.defaults.baseURL = "https://suhail-api.lilash.dev";
+// axios.defaults.baseURL = "http://127.0.0.1:4000";
 export default axios
